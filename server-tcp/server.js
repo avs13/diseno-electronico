b3e485b387d76534a86c5f57cc6b7c7a577f704c
Oscar Bustillo <oscardavidbustillo@gmail.com>
2023-08-26 19:59:43 -0500

    .

--- server-tcp/server.js
+++ server-tcp/server.js
@@ -56,56 +56,6 @@ server.listen(8000, () => {
     console.log("Server TCP listening on port", server.address().port);
 });
 
-app.get("/", (req, res) => {
-    res.send(`<!DOCTYPE html>
-    <html lang="en">
-        <head>
-            <title>Warp Chat</title>
-        </head>
-        <body>
-            <h1>Warp chat</h1>
-            <div id="chat">
-                <p><em>Connecting...</em></p>
-            </div>
-            <input type="text" id="text" />
-            <button type="button" id="send">Send</button>
-            <script type="text/javascript">
-            const chat = document.getElementById('chat');
-            const text = document.getElementById('text');
-            const uri = 'ws://' + location.host + '';
-            const ws = new WebSocket(uri);
-    
-            function message(data) {
-                const line = document.createElement('p');
-                line.innerText = data;
-                chat.appendChild(line);
-            }
-    
-            ws.onopen = function() {
-                chat.innerHTML = '<p><em>Connected!</em></p>';
-            };
-    
-            ws.onmessage = function(msg) {
-                console.log(msg.data);
-                message(msg.data);
-            };
-    
-            ws.onclose = function() {
-                chat.getElementsByTagName('em')[0].innerText = 'Disconnected!';
-            };
-    
-            send.onclick = function() {
-                const msg = text.value;
-                ws.send(msg);
-                text.value = '';
-    
-                message('<You>: ' + msg);
-            };
-            </script>
-        </body>
-    </html>`)
-});
-
 
 httpServer.listen(8001, () => {
     console.log("Server HTTP listening on port", 8001);
