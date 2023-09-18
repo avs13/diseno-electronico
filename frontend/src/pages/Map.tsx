ec21c45983819a292720544c52d4b95bbbb03bec
Daniela Campo <143294907+didictrlz@users.noreply.github.com>
2023-09-17 22:23:17 -0500

    rest. titulo

--- frontend/src/pages/Map.tsx
+++ frontend/src/pages/Map.tsx
@@ -45,7 +45,7 @@ export const Map = () => {
   return (
     <div className="w-full h-full text-slate-800  flex flex-col gap-3">
       <div className=" border-solid border border-slate-300 rounded-lg p-2">
-        <p className="font-semibold text-xl text-center">My GPS</p>
+        <p className="font-semibold text-xl text-center">MY GPS</p>
         <div className="flex justify-between w-4/12">
           <div>
             <p>
